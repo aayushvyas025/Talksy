62c64e60f8fd1275d7e3b9f3340290ab48f2e4c2
Aayush Vyas <aayush25vyas@gmail.com>
2025-11-09 21:44:54 +0530

    Fix the static serve frontend

--- backend/src/helper/utils/servingStaticFrontend.js
+++ backend/src/helper/utils/servingStaticFrontend.js
@@ -4,16 +4,16 @@ import express from "express";
 
 const { envVariables } = commonConstants;
 const { nodeEnviornment } = envVariables;
+const __dirname = path.resolve();
 
 function servingStaticFrontend(app) {
-  const __dirname = path.resolve();
-  if (nodeEnviornment === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    console.log("Serving static from:", path.join(__dirname, "../frontend/dist"));
-    app.use((_, response) => {
-      response.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
-    });
+    if(nodeEnviornment === "production") {
+     app.use(express.static(path.join(__dirname, "../frontend/dist"))); 
+
+     app.use((_, response) => {
+        response.sendFile(path.join(__dirname, "../frontend/dist", "index.html"))
+     })
+    }
   }
-}
 
 export default servingStaticFrontend;
