2d1f34ba5e1b5cc23fdda8e00cc3d70f0aa4ee04
Aayush Vyas <aayush25vyas@gmail.com>
2026-08-17 00:21:22 +0530

    Working on protectRoute middleware of auth

--- server/src/routes/users/user.route.js
+++ server/src/routes/users/user.route.js
@@ -1,10 +1,11 @@
 import apiRoutes from "#constant/apiRoutes.constant";
 import { updateProfile } from "#controller/user/user.controller";
-import express from "express" 
+import { protectRoute } from "#middleware/auth/auth.middleware";
+import express from "express";
 
-const {update_profile} = apiRoutes.user; 
-const router = express.Router(); 
+const { update_profile } = apiRoutes.user;
+const router = express.Router();
 
-router.put(update_profile, updateProfile); 
+router.put(update_profile, protectRoute, updateProfile);
 
-export default router
+export default router;
