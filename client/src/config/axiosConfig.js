59967a34a57e2397058ce2aaf6083c7592147f8a
Aayush Vyas <aayush25vyas@gmail.com>
2026-09-05 02:38:57 +0530

    configure the ts-type alias importing

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -1,10 +1,10 @@
-// import axios from "axios";
-// import envConfig from "../constant/envConfig";
+import axios from "axios";
+import envConfig from "@/constant/envConfig";
 
-// const { applicationMode, backendBaseUrl } = envConfig;
+const { applicationMode, backendBaseUrl } = envConfig;
 
-// const API = axios.create({
-//   baseURL: applicationMode === "production" ? "/talksy/api/v1" : backendBaseUrl,
-// });
+const API = axios.create({
+  baseURL: applicationMode === "production" ? "/talksy/api/v1" : backendBaseUrl,
+});
 
-// export default API;
+export default API;
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./src"),
+      "@components": path.resolve(import.meta.dirname, "./src/components"),
+      "@utils": path.resolve(import.meta.dirname, "./src/utils"),
+      "@constant": path.resolve(import.meta.dirname, "./src/constant"),
+    },
+  },
+});
