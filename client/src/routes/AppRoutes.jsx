747aca6627234200f2b7a52cdb399223c46c22a3
Aayush Vyas <aayush25vyas@gmail.com>
2026-09-07 00:01:28 +0530

    implement the protected-route feature on client-side

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,7 +1,27 @@
 import { Route, Routes } from "react-router-dom";
 import { AuthPage, HomePage, ProfilePage, SettingPage } from "@/pages";
+import useAuthStore from "@/store/auth/authStore";
+import { useEffect } from "react";
 
 function AppRoutes() {
+  const { authUser, checkAuth } = useAuthStore();
+
+  async function handleAuthChecking() {
+    try {
+      const { success, message } = await checkAuth();
+      if (!success) {
+        console.error(message);
+      }
+    } catch (error) {
+      console.error(`Error, checking auth: ${error.message}`);
+      throw error;
+    }
+  }
+
+  useEffect(() => {
+    handleAuthChecking();
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
