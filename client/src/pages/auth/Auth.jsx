6d19775b85ced002fa9d9a2fe25599754c27cb67
Aayush Vyas <aayush25vyas@gmail.com>
2026-09-07 00:48:33 +0530

    Working on auth-page and auth-store

--- client/src/pages/auth/Auth.jsx
+++ client/src/pages/auth/Auth.jsx
@@ -1,11 +1,15 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useState } from "react";
 
 function Auth() {
-  return (
-    <Suspense>
-        Auth Page 
-    </Suspense>
-  )
+  const [authState, setAuthState] = useState("signup");
+  const [showPassword, setShowPassword] = useState(false);
+  const [signupUser, setSignupUser] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  return <Suspense>Auth Page</Suspense>;
 }
 
-export default Auth 
+export default Auth;
