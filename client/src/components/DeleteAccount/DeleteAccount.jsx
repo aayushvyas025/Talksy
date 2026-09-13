e032fc74326edaf5d8eacb25973568c1c4736e18
Aayush Vyas <aayush25vyas@gmail.com>
2026-09-13 12:55:28 +0530

    Adding deleteUser functionality

--- client/src/components/DeleteAccount/DeleteAccount.jsx
+++ client/src/components/DeleteAccount/DeleteAccount.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import Button from '../Button/Button'
-import { Trash2 } from 'lucide-react'
+import React from "react";
+import Button from "../Button/Button";
+import useAuthStore from "@/store/auth/authStore";
 
 function DeleteAccount() {
+  const { isUserDeleted, userAccountDelete } = useAuthStore();
   return (
-     <div className='flex items-center justify-between py-2'> 
-                <span>Delete Account</span> 
-                <Button style={"text-red-400"}>
-                    <Trash2 className='w-4 h-4' />
-                </Button>
-            </div>
-  )
+    <div className="flex items-center justify-between py-2">
+      <span>Delete Account</span>
+      <Button
+        style={"text-red-300 hover:text-red-600"}
+        onClick={userAccountDelete}
+        disabled={isUserDeleted}
+      >
+        Delete
+      </Button>
+    </div>
+  );
 }
 
-export default DeleteAccount
+export default DeleteAccount;
