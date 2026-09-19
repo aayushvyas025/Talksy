0608444b235db3c5375993a93950664f9be750c4
Aayush Vyas <aayush25vyas@gmail.com>
2026-09-20 01:36:25 +0530

    MessageInputPreview Component

--- client/src/components/MessageInputPreview/MessageInputPreview.jsx
+++ client/src/components/MessageInputPreview/MessageInputPreview.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import { Send } from "lucide-react";
+import React from "react";
 
 function MessageInputPreview() {
   return (
-    <div className='p-4 border-t border-base-300 bg-base-100'>
+    <div className="p-4 border-t border-base-300 bg-base-100">
       <div className="flex gap-2">
-        <input type='text' className='input input-bordered flex-1 text-sm h-10' placeholder='Type a message' value={"This is a Preview"} readOnly />
+        <input
+          type="text"
+          className="input input-bordered flex-1 text-sm h-10"
+          placeholder="Type a message"
+          value={"This is a Preview"}
+          readOnly
+        />
+        <button className="btn btn-primary h-10 min-h-0">
+          <Send size={18} />
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default MessageInputPreview
+export default MessageInputPreview;
